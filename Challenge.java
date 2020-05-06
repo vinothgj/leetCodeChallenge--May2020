7771105b44e8bfa090d86531db196d05fb5a96fe
vinothgj <43566152+vinothgj@users.noreply.github.com>
2020-05-06 13:59:28 +0530

    Add code for challenge 4
    Number Complement

--- Challenge.java
+++ Challenge.java
@@ -4,41 +4,51 @@ package com.codechallenge.may;
 public class Challenge {
 
     public static void main(String[] args) {
-
+        findComplement(5);
     }
 
-    public int findComplement(int num) {
+    /**
+     * Number Complement
+     * Solution
+     * Given a positive integer, output its complement number. The complement strategy is to flip the bits of its binary representation.
+     * Example 1:
+     * Input: 5
+     * Output: 2
+     * Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010. So you need
+     *
+     * @param num
+     * @return
+     */
+    public static int findComplement(int num) {
 
         String binaryValue = Integer.toBinaryString(num);
-        char[] binaryValueArray = binaryValue.toCharArray();
         String toBinary = "";
 
-        for (int i = 0; i < binaryValueArray.length; i++) {
-            if (binaryValueArray[i] == 0) {
-                toBinary += "1";
-            } else {
-                toBinary += "0";
+        for (int i = 0; i < binaryValue.length(); i++) {
+            if (binaryValue.charAt(i) == '0') {
+                toBinary = toBinary + "1";
+            } else if (binaryValue.charAt(i) == '1') {
+                toBinary = toBinary + "0";
             }
         }
 
-        //String returnValue = Integer.toBinaryString(Integer.parseInt(toBinary));
-
-        return Integer.parseInt(Integer.toBinaryString(Integer.parseInt(toBinary)));
+        return Integer.parseInt(toBinary, 2);
     }
 
     /**
      * First Unique Character in a String
      * Solution
      * Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
-     *
+     * <p>
      * Examples:
-     *
+     * <p>
      * s = "leetcode"
      * return 0.
-     *
+     * <p>
      * s = "loveleetcode",
      * return 2.
-     * Note: You may assume the string contain only lowercase letters.  
+     * Note: You may assume the string contain only lowercase letters.
+     *
      * @param s
      * @return
      */
